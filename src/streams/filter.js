b43feb410b7e094f4fc3d65e455ebae45f7ab2ec
Alex <alex.devenrgy@gmail.com>
2026-03-08 14:44:48 +0300

    feat: Add line filtering via CLI pattern

--- src/streams/filter.js
+++ src/streams/filter.js
@@ -1,9 +1,47 @@
+import { Transform } from "node:stream";
+import { parseArgs } from "node:util";
+
 const filter = () => {
-  // Write your code here
-  // Read from process.stdin
-  // Filter lines by --pattern CLI argument
-  // Use Transform Stream
-  // Write to process.stdout
+	const {
+		values: { pattern },
+	} = parseArgs({
+		options: {
+			pattern: {
+				type: "string",
+			},
+		},
+	});
+
+	if (!pattern) {
+		process.stderr.write("Error: --pattern <string> argument required\n");
+		process.exit(1);
+	}
+
+	let buffer = "";
+
+	const transform = new Transform({
+		transform(chunk, _, callback) {
+			buffer += chunk.toString();
+			const lines = buffer.split("\n");
+			buffer = lines.pop();
+
+			for (const line of lines) {
+				if (line.includes(pattern)) {
+					this.push(`${line}\n`);
+				}
+			}
+
+			callback();
+		},
+		flush(callback) {
+			if (buffer?.includes(pattern)) {
+				this.push(`${buffer}\n`);
+			}
+			callback();
+		},
+	});
+
+	process.stdin.pipe(transform).pipe(process.stdout);
 };
 
 filter();
